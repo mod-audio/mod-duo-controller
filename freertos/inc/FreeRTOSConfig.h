d6423f1932b2e7efb959bd5512fabe672575c954
Ricardo Crudo <ricardo.crudo@gmail.com>
2013-10-09 16:08:15 -0300

    configuration organized

--- freertos/inc/FreeRTOSConfig.h
+++ freertos/inc/FreeRTOSConfig.h
@@ -88,56 +88,75 @@
  * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK			0
-#define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
-#define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) SystemCoreClock )
-#define configTICK_RATE_HZ			( ( portTickType ) 1000 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 80 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 24 * 1024 ) )
-#define configMAX_TASK_NAME_LEN		( 12 )
-#define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		0
-#define configIDLE_SHOULD_YIELD		0
-#define configUSE_CO_ROUTINES 		0
-#define configUSE_MUTEXES			0
-#define configUSE_MALLOC_FAILED_HOOK    1
+#define configUSE_PREEMPTION                1
+#define configMAX_PRIORITIES                ( ( unsigned portBASE_TYPE ) 5 )
+#define configCPU_CLOCK_HZ                  ( ( unsigned long ) SystemCoreClock )
+#define configTICK_RATE_HZ                  ( ( portTickType ) 1000 )
+#define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 80 )
+#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 24 * 1024 ) )
+#define configMAX_TASK_NAME_LEN             ( 12 )
+#define configUSE_16_BIT_TICKS              0
+#define configIDLE_SHOULD_YIELD             0
+#define configUSE_CO_ROUTINES               0
+#define configUSE_MUTEXES                   0
+#define configUSE_COUNTING_SEMAPHORES       1
+#define configUSE_ALTERNATIVE_API           0
+#define configUSE_RECURSIVE_MUTEXES         0
+#define configQUEUE_REGISTRY_SIZE           10
+#define configUSE_QUEUE_SETS                0
+#define configUSE_TIME_SLICING              0
+#define configUSE_NEWLIB_REENTRANT          0
 
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+/* Hook function related definitions. */
+#define configUSE_IDLE_HOOK                 0
+#define configUSE_TICK_HOOK                 0
+#define configCHECK_FOR_STACK_OVERFLOW      1
+#define configUSE_MALLOC_FAILED_HOOK        1
 
-#define configUSE_COUNTING_SEMAPHORES 	1
-#define configUSE_ALTERNATIVE_API 		0
-#define configCHECK_FOR_STACK_OVERFLOW	1
-#define configUSE_RECURSIVE_MUTEXES		0
-#define configQUEUE_REGISTRY_SIZE		10
-#define configGENERATE_RUN_TIME_STATS	0
+/* Run time and task stats gathering related definitions. */
+#define configGENERATE_RUN_TIME_STATS       0
+#define configUSE_TRACE_FACILITY            0
 
-/* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
+/* Co-routine related definitions. */
+#define configUSE_CO_ROUTINES               0
+#define configMAX_CO_ROUTINE_PRIORITIES     1
 
-#define INCLUDE_vTaskPrioritySet			0
-#define INCLUDE_uxTaskPriorityGet			0
-#define INCLUDE_vTaskDelete					1
-#define INCLUDE_vTaskCleanUpResources		0
-#define INCLUDE_vTaskSuspend				0
-#define INCLUDE_vTaskDelayUntil				1
-#define INCLUDE_vTaskDelay					1
-#define INCLUDE_uxTaskGetStackHighWaterMark	1
+/* Software timer related definitions. */
+#define configUSE_TIMERS                    1
+#define configTIMER_TASK_PRIORITY           3
+#define configTIMER_QUEUE_LENGTH            10
+#define configTIMER_TASK_STACK_DEPTH        configMINIMAL_STACK_SIZE
+
+/* Optional functions - most linkers will remove unused functions anyway. */
+#define INCLUDE_vTaskPrioritySet                0
+#define INCLUDE_uxTaskPriorityGet               0
+#define INCLUDE_vTaskDelete                     1
+#define INCLUDE_vTaskCleanUpResources           0
+#define INCLUDE_vTaskSuspend                    0
+#define INCLUDE_vTaskDelayUntil                 1
+#define INCLUDE_vTaskDelay                      1
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
+#define INCLUDE_xTaskGetSchedulerState          0
+#define INCLUDE_xTaskGetCurrentTaskHandle       0
+#define INCLUDE_xTaskGetIdleTaskHandle          0
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle  0
+#define INCLUDE_pcTaskGetTaskName               0
+#define INCLUDE_eTaskGetState                   0
+
 
 /* Use the system definition, if there is one */
 #ifdef __NVIC_PRIO_BITS
-	#define configPRIO_BITS       __NVIC_PRIO_BITS
+    #define configPRIO_BITS       __NVIC_PRIO_BITS
 #else
-	#define configPRIO_BITS       5        /* 32 priority levels */
+    #define configPRIO_BITS       5        /* 32 priority levels */
 #endif
 
 /* The lowest priority. */
-#define configKERNEL_INTERRUPT_PRIORITY 	( 31 << (8 - configPRIO_BITS) )
+#define configKERNEL_INTERRUPT_PRIORITY         ( 31 << (8 - configPRIO_BITS) )
 /* Priority 5, or 160 as only the top three bits are implemented. */
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( 5 << (8 - configPRIO_BITS) )
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    ( 5 << (8 - configPRIO_BITS) )
 
 /* Priorities passed to NVIC_SetPriority() do not require shifting as the
 function does the shifting itself.  Note these priorities need to be equal to
