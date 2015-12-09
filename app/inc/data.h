448abe4cde172af8aec61f99a43f8041bc3cf66d
Ricardo Crudo <ricardo.crudo@gmail.com>
2015-12-09 13:20:00 -0200

    bug fix: step can be greater than 127

--- app/inc/data.h
+++ app/inc/data.h
@@ -54,7 +54,7 @@ typedef struct CONTROL_T {
     uint8_t properties_mask;
     uint8_t properties;
     float value, minimum, maximum;
-    int8_t step, steps;
+    int32_t step, steps;
     uint8_t controls_count, control_index;
     uint8_t scale_points_count;
     scale_point_t **scale_points;
