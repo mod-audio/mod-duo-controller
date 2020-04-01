281b3a63d4ae05ae39c4263bf5677fb2548dba47
Jan <jan@moddevices.com>
2020-04-01 14:29:47 +0200

    fix warnings of new compiler

--- app/src/system.c
+++ app/src/system.c
@@ -412,7 +412,7 @@ void system_bluetooth_cb(void *arg, int event)
         {
             response = cli_command("mod-bluetooth hmi", CLI_RETRIEVE_RESPONSE);
             
-            strncpy(resp, response, sizeof(resp));
+            strncpy(resp, response, sizeof(resp)-1);
             char **items = strarr_split(resp, '|');;
 
             if (items)
